0a5035dd321087560dfcd6144dc2f320922e49ef
Steve Ko <stevko@buffalo.edu>
2020-02-05 18:23:53 -0500

    Initial commit for a memory allocator

--- bl32/fsp/kernel/src/lib.rs
+++ bl32/fsp/kernel/src/lib.rs
@@ -1,16 +1,28 @@
+//!
+//! This has a main entry function fsp_main().
+//!
 #![no_std]
+#![feature(alloc_error_handler)] // for our own allocator implementation
 
+#[rustfmt::skip] // the log module defines macros used by fsp_allocator, so it has to come first.
 mod log;
+mod fsp_allocator;
 
+extern crate alloc; // need this due to #![no_std]---for regular Rust, it is by default.
+
+use alloc::boxed::Box;
 use core::panic::PanicInfo;
 
 /// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    debug!("Panic");
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn fsp_main() {
     debug!("fsp debug");
+    let _x = Box::new(0);
+    debug!("Used Box");
 }
--- bl32/fsp/kernel/src/log.rs
+++ bl32/fsp/kernel/src/log.rs
@@ -1,10 +1,11 @@
-/*
- * These macros print output to the console. They are very simple at this point and just
- * accept a single string literal and uses libc's printf.
- *
- */
+//!
+//! These macros print output to the console. They are very simple at this point and just
+//! accept a single string literal and uses libc's printf.
+//!
+#![macro_use]
 
 #[cfg(feature = "debug")]
+#[allow(dead_code)] // since this won't be used if debug! is not called anywhere
 extern "C" {
     pub fn printf(fmt: *const u8, ...);
 }
@@ -14,9 +15,10 @@ macro_rules! debug {
     ( $x:expr ) => {
         #[cfg(feature = "debug")]
         {
+            #[allow(unused_unsafe)] // to avoid nested unsafe warnings
             unsafe {
                 crate::log::printf(
-                    concat!("FSP DEBUG: ", $x, "\n", '\0').as_bytes().as_ptr() as *const u8
+                    concat!("FSP DEBUG: ", $x, '\n', '\0').as_bytes().as_ptr() as *const u8
                 );
             }
         }
